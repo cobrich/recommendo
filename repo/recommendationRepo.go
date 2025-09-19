7ee1c09d0ba8a43b33c5b591c69d14cfb6b6432c
cobrich <tursunbekzat07@gmail.com>
2025-09-19 06:46:12 +0500

    feat(recommendation): implement API for creating and viewing recommendations

--- repo/recommendationRepo.go
+++ repo/recommendationRepo.go
@@ -1,6 +1,12 @@
 package repo
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/cobrich/recommendo/models"
+)
 
 type RecommendationRepo struct {
 	DB *sql.DB
@@ -10,3 +16,48 @@ func NewRecommendationRepo(db *sql.DB) *RecommendationRepo {
 	return &RecommendationRepo{DB: db}
 }
 
+func (r *RecommendationRepo) GetRecommendation(ctx context.Context, fromId, toID, mediaID int) error {
+	var recommendation models.Recommendation
+
+	query := `SELECT recommendation_id, from_user_id, to_user_id, media_id, created_at FROM recommendations 
+	WHERE from_user_id=$1 and to_user_id=$2 and media_id=$3`
+
+	if err := r.DB.QueryRowContext(ctx, query, fromId, toID, mediaID).Scan(
+		&recommendation.ID,
+		&recommendation.FromUserID,
+		&recommendation.ToUserID,
+		&recommendation.MediaID,
+		&recommendation.CreatedAt); err != nil {
+		if err == sql.ErrNoRows {
+			return sql.ErrNoRows
+		}
+		return fmt.Errorf("failed to get recommendation: %w", err)
+	}
+
+	return nil
+}
+
+func (r *RecommendationRepo) CreateRecommendation(ctx context.Context, fromId, toID, mediaID int) error {
+	query := `
+        INSERT INTO recommendations (from_user_id, to_user_id, media_id)
+        VALUES ($1, $2, $3)
+		`
+
+	result, err := r.DB.ExecContext(ctx, query, fromId, toID, mediaID)
+	if err != nil {
+		// Если произошла ошибка (например, нарушение UNIQUE constraint), мы ее получим.
+		return fmt.Errorf("failed to create recommendation: %w", err)
+	}
+
+	// 3. (Опционально, но хорошая практика) Проверяем, что была затронута ровно одна строка.
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		// Маловероятная ошибка, но проверка не помешает.
+		return fmt.Errorf("failed to check rows affected: %w", err)
+	}
+	if rowsAffected == 0 {
+		return fmt.Errorf("no rows were affected, following not created")
+	}
+
+	return nil
+}
--- service/recommendationService.go
+++ service/recommendationService.go
@@ -1,11 +1,68 @@
 package service
 
-import "github.com/cobrich/recommendo/repo"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/cobrich/recommendo/repo"
+)
 
 type RecommendationService struct {
-	r *repo.RecommendationRepo
+	// Собственные зависимости (репозитории)
+	r         *repo.RecommendationRepo
+	mediaRepo *repo.MediaRepo // Допустим, он может сам создавать медиа
+
+	// Зависимости от ДРУГИХ СЕРВИСОВ
+	userService   *UserService
+	followService *FollowService
 }
 
-func NewRecommmendationService(r *repo.RecommendationRepo) *RecommendationService {
-	return &RecommendationService{r: r}
+// Конструктор теперь принимает все нужные зависимости
+func NewRecommendationService(rRepo *repo.RecommendationRepo, mRepo *repo.MediaRepo, uService *UserService, fService *FollowService) *RecommendationService {
+	return &RecommendationService{
+		r:             rRepo,
+		mediaRepo:     mRepo,
+		userService:   uService,
+		followService: fService,
+	}
+}
+
+func (s *RecommendationService) CreateRecommendation(ctx context.Context, fromID, toID, mediaID int) error {
+	// 1. Check existance of users
+	_, err := s.userService.GetUserByID(ctx, fromID)
+	if err != nil {
+		return err
+	}
+	_, err = s.userService.GetUserByID(ctx, toID)
+	if err != nil {
+		return err
+	}
+
+	// 2. Check existance of media
+	_, err = s.mediaRepo.GetMedia(ctx, mediaID)
+	if err != nil {
+		return err
+	}
+
+	// 3. Check if users are friends
+	areFriends, err := s.followService.AreUsersFriends(ctx, fromID, toID)
+	if err != nil {
+		return err
+	}
+	if !areFriends {
+		return fmt.Errorf("users are not friends")
+	}
+
+	// 4. Check is it recommandation first time
+	err = s.r.GetRecommendation(ctx, fromID, toID, mediaID)
+	if err == nil {
+		return fmt.Errorf("this media has already been recommended to this user")
+	}
+	if err != sql.ErrNoRows {
+		return fmt.Errorf("unexpected error when checking for existing recommendation: %w", err)
+	}
+
+	// 5. If not exists, and there is no problems create recomm
+	return s.r.CreateRecommendation(ctx, fromID, toID, mediaID)
 }
